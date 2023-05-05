b4e803aba80ce4bf64724fe15cf053d1ce2427a4
camVasseur <camille23.vasseur@gmail.com>
2023-05-05 10:44:55 +0200

    feat(TodoAppDatabase): Add SQLite DB management project

---
+++ EcoleIt.ToDoApp.Sqlite/Repositories/TodoItemRepository.cs
@@ -0,0 +1,13 @@
+﻿using EcoleIt.ToDoApp.Core.Outputs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcoleIt.ToDoApp.Sqlite.Repositories
+{
+    public class TodoItemRepository : ITodoItemRepository
+    {
+    }
+}
